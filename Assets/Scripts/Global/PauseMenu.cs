2c38120bc9e08a1247953f0256589b0e0c896b2c
GreenShark777 <greenshark1740@gmail.com>
2021-09-18 16:26:47 +0200

    Nuova repository

---
+++ Assets/Scripts/Global/PauseMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    //riferimento al menù di pausa
+    private GameObject pauseMenu;
+
+    private void Start()
+    {
+        //ottiene il riferimento al menù di pausa
+        pauseMenu = transform.GetChild(0).gameObject;
+
+    }
+
+    public void Pause(bool isPaused)
+    {
+        //se bisogna mettere in pausa, il tempo viene fermato
+        if (isPaused) { Time.timeScale = 0; }
+        //altrimenti, viene fatto scorrere normalmente
+        else { Time.timeScale = 1; }
+        //il menù di pausa viene attivato o disattivato in base al parametro ricevuto
+        pauseMenu.SetActive(isPaused);
+
+    }
+
+}
