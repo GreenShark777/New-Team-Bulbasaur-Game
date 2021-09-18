fce9ed5555b4893d00e2d50b6babc50c3be10a97
GreenShark777 <greenshark1740@gmail.com>
2021-09-18 18:43:08 +0200

    Aggiunti sprite per animazione di maiale, marionetta e giocatore e iniziato skinning

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+//Si occupa del movimento e fisica del giocatore
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //indica quanto velocemente si muove il giocatore
+    [SerializeField]
+    private float speed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
